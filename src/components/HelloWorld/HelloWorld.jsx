238d3dbfab7901732f47db69d56e9d042fcea085
Juan <juan_romaris@hotmail.com>
2017-07-15 14:36:06 -0300

    cambio

--- src/components/HelloWorld/HelloWorld.jsx
+++ src/components/HelloWorld/HelloWorld.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-export default class Chat extends React.Component {
-    constructor (props){
+export default class HelloWorld extends React.Component {
+    
+     constructor (props){
         //super es la forma del hijo de comunicar al padre que existe
         super(props);
 
         this.state = {
-            message:'Hola Mundo',
-            
+            message:'hola mundo'
+           
         }
     }
-    
+
+
     render(){
         return (
-            <div>
+            
                 <div>{this.state.message}</div>
                
-            </div>
+            
         );
     }
 }
--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 //Components
-import HelloWorld from './components/HolaMundo/HelloWorld';
+import HelloWorld from './components/HelloWorld/HelloWorld';
 
 
 
-ReactDOM.render(<HelloWold/>, document.querySelector('#example'));
+ReactDOM.render(<HelloWorld/>, document.querySelector('#example'));
 
